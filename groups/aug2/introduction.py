15611335266b4910f29a5cc362cb779cfca7307d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-08-02 20:01:55 +0300

    introduction

---
+++ groups/aug2/introduction.py
@@ -0,0 +1,61 @@
+print("Hello August World!")
+# print("We can do something else")
+# by default we print to standard output - we can change that later
+# we use # for comments
+# we write comments for humans not computers - computers ignore comments
+# comments should clarify intent - why are we doing something
+
+# let's try to break some things
+print("hmmmm")
+# we can print Latvian
+print("Varu stāstīt un rakstīt Latviski par runčiem un kaķiem..")
+# Python 3 has full Unicode support
+# I can put text in any language
+# I can use Emoji even
+print("Es varu baudīt arī alu.. 🍺 or even 🍻 with friends")
+
+# simple arithmetic
+print(2+2)
+# compare
+print("2+2") # this is text string
+#
+print(10-3)
+print(7*10) # * for multiplication
+print(10/4) # so / returns division
+print(10//4) # gives you whole (integer) result
+# very import is reminder (similar to modulo)
+print(20%7) # we use reminder quite a bit for example for odd or even
+# when reminder of division of 2
+print(10%2,11%2,12%2,13%2)
+
+
+# notice we used , to separate results
+print("When I calculate 2*2 I get", 2*2) # , by default will give you one space
+
+# we can add strings together as well
+print("Valdis likes to" + " teach") # string concatenation
+
+# we can multiply strings with numbers!
+print("Beer"*5)
+print("All work and no play makes Jack a dull boy\n"*3)
+# notice \n that stands for newline
+
+# ** we can use it to raise to power
+print(2**8) # so 8 bits come into 1 byte whichs
+# can hold 256 different values
+# 16 bit
+print(2**16)
+# 24 bit color is..
+print("24 bit color can hold this many different values", 2**24)
+# 32 bit system memory limit
+print("32 bit computers can easily acces this many mmeory bits", 2**32)
+# so 64 bits to rescue
+print("64 bit addresses have plenty of space", 2**64)
+
+# normal parenthesis for math
+print("(2+2)*(3+7) is.", (2+2)*(3+7))
+
+
+
+
+
