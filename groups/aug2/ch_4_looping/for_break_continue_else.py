ec63c7dfea23d8f00b5826f4bbf901f7088f21e1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-08-09 19:44:29 +0300

    for break continue

--- groups/aug2/ch_4_looping/for_break_continue_else.py
+++ groups/aug2/ch_4_looping/for_break_continue_else.py
@@ -1,7 +1,8 @@
 # for loops have same constructs as while loops
 # break, continue and also else
 
-for n in range(5):
+for n in range(5): # so we go through 0,1,2,3,4 
+    
     print(n)
     if n == 3:
         print("Break time!", n)
@@ -9,3 +10,31 @@ for n in range(5):
 
 # here we go after break
 print("After break n is", n)
+
+# continue is less used since we can simulate it easily with else
+for n in range(10):
+    if n % 2 == 0:
+        print("Even", n)
+        continue # go to start
+    # again we could have used else here
+    print("Odd", n, n**2)
+    
+# finally we have less used else attached to for loop
+
+total = 0
+for n in range(100):
+    total += n**2 # we are adding squares
+    print("On number", n)
+    print("Total is", total)
+    user_input = input("Do you want to quit (y/n)?")
+    if user_input == "y":
+        print("Quitting time when n is", n)
+        break
+    # else: # not really needed
+    print("Still on n", n)
+else: # attached to for loop, runs when loop finishes normally
+    print("Whew we finished loop normally")
+    # so above will not run if we break
+    
+print("outside for loop")
+    
