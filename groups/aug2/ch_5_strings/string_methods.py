ec8be86f603dc3941e9073aaa36ba852c9764b3e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-08-14 19:23:53 +0300

    string methods

--- groups/aug2/ch_5_strings/string_methods.py
+++ groups/aug2/ch_5_strings/string_methods.py
@@ -55,3 +55,87 @@ except ValueError:
 
 # thus you can check for -1 using find, or use try except with index method
 
+a_string = "aaaaa"
+# we can use count method to count how many times a substring appears
+print(a_string.count("a")) # so we get 5
+# however it is skipped if we have overlapping substrings
+print(a_string.count("aa")) # so we get 2 not 4
+
+# we can use replace method to replace all occurrences of a substring
+food = "auzu putra ar kartupeli"
+# so let's replace 2nd letter with y
+try:
+    food[1] = "y" # this will not work
+except TypeError:
+    print("Strings are immutable, we cannot change them")
+
+# instead I could have used replace method
+print(food.replace("u", "y")) # so we get ayzy pytra ar kartypeli
+# again original food is not changed
+# so i would need to assign it to a new variable
+new_food = food.replace("u", "y")
+print(new_food)
+# if I only want to replace first two occurrences
+print(food.replace("u", "y", 2)) # so we get ayzy putra ar kartupeli
+
+# we could have replaced a word as well
+# lets change putra to tume
+print(food.replace("putra", "tume")) # so we get auzu tume ar kartupeli
+# again use variable assignment if you want to keep the change
+
+# I can use slicing with new text
+# so let's make Baltā putra
+print(food.replace("auzu", "Baltā"))
+print(food.replace("auzu", "Baltā")[:11]) # so we get Baltā putra
+# i could have use slicing and concatenation
+print("Baltā" + food[4:]) # so we get Baltā putra ar kartupeli
+
+# in real life off by one errors are very common
+# easy to fix as well adjust offset by 1
+
+# so let's look at some more string methods
+dogs = "Džulis, Reksis, Šariks, Džeks, mazais Džekijs arī"
+# let's make a string all lowercase
+print(dogs.lower()) # so we get džulis, reksis, šariks
+# let's make a string all uppercase - YELLING!
+print(dogs.upper()) # so we get DŽULIS, REKSIS, ŠARIKS, DŽEKS
+# again if we want to save it we need to assign it to a variable
+yelling_dogs = dogs.upper()
+print(yelling_dogs)
+# Capitalize will make first letter uppercase
+print(dogs.capitalize()) # so we get Džulis, reksis, šariks, džeks, mazais džekijs
+# so only first letter is capitalized
+# we also have title method which will capitalize all words
+print(dogs.title()) # so we get Džulis, Reksis, Šariks, Džeks, Mazais Džekijs
+# we have a swapcase method which will swap cases
+print(dogs.swapcase()) # so we get dŽULIS, rEKŠIS, šARIKS, dŽEKS, MAZAIS dŽEKIJS ARĪ
+
+# we can use startswith method to check if a string starts with a substring
+print(dogs.startswith("Džulis")) # so we get True
+# we can use endswith method to check if a string ends with a substring
+print(dogs.endswith("arī")) # so we get True
+
+# we can combine these with if statements
+# for example I want to check if a string starts with dž and ignore case
+if dogs.lower().startswith("dž"): # so I made a copy of dogs and made it lowercase and checked if it starts with dž
+    print("We have a dž dog")
+else:
+    print("We do not have a dž dog")
+
+# we can also use some cleanup methods
+dirty_city = "  Rīga\t \n  "
+print(dirty_city)
+# we can clean it using strip method
+clean_city = dirty_city.strip()
+print(clean_city)
+# we can only clean left side with lstrip
+print(dirty_city.lstrip())
+# or right side with rstrip
+print(dirty_city.rstrip())
+
+# i could apply these string methods to string literal
+
+
+
+
+
