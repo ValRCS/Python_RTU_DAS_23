0b64b6a8912940fa3e8efb7c78fb7ad3dac3e78c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2023-08-30 21:06:24 +0300

    exercise solutions

---
+++ groups/aug30/day1/uzd1.py
@@ -0,0 +1,14 @@
+# My name 5 times
+print("Valdis"*5)
+ 
+# Name * 7
+print("Valdis"*7) 
+ 
+# Seconds in one year
+print(365*24*60*60)
+ 
+# Google number
+print(10**100)
+ 
+#Bananana using only Ba and na
+print("Ba" + " na "*3)
